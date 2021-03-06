d52cbbd5f0fb5d91d4844e599704831b5121a682
bopus <65352524+bopus@users.noreply.github.com>
2021-03-06 14:31:04 -0500

    added unban but does not work properly

--- src/commands/moderation/BanCommand.js
+++ src/commands/moderation/BanCommand.js
@@ -8,16 +8,16 @@ module.exports = class BanCommand extends BaseCommand {
 
   async run(client, message, args) {
     //Permission Checks
-    if(!message.member.hasPermission("BAN_MEMBERS")) return message.channel.send("You do not have the required permissions to use this command.")
-    if (!message.guild.me.hasPermission("BAN_MEMBERS")) return message.channel.send("Bot does not have the requires permissions to use this command.")
+    if(!message.member.hasPermission("BAN_MEMBERS")) return message.channel.send("You do not have the required permissions to use this command.").then(msg => msg.delete({timeout: 7000})).then(message.delete({timeout: 7000}));
+    if (!message.guild.me.hasPermission("BAN_MEMBERS")) return message.channel.send("Bot does not have the requires permissions to use this command.").then(msg => msg.delete({timeout: 7000})).then(message.delete({timeout: 7000}));
     //Variables
     let reason = args.slice(1).join(" ");
     const mentionedMember = message.mentions.members.first();
     //Input Checks
     if (!reason) reason = "No reason was provided.";
-    if (!args[0]) return message.channel.send("You must mention a user to ban. \`.ban @user reason\`");
-    if (!mentionedMember) return message.channel.send("That user does not exist in this server.");
-    if (!mentionedMember.bannable) return message.channel.send("That user is not bannable.");
+    if (!args[0]) return message.channel.send("You must mention a user to ban. \`.ban @user reason\`").then(msg => msg.delete({timeout: 7000})).then(message.delete({timeout: 7000}));
+    if (!mentionedMember) return message.channel.send("That user does not exist in this server.").then(msg => msg.delete({timeout: 7000})).then(message.delete({timeout: 7000}));
+    if (!mentionedMember.bannable) return message.channel.send("That user is not bannable.").then(msg => msg.delete({timeout: 7000})).then(message.delete({timeout: 7000}));
     //Executing
     const banEmbed = new Discord.MessageEmbed()
       .setTitle(`You have been banned from ${message.guild.name}`)
--- src/commands/moderation/UnbanCommand.js
+++ src/commands/moderation/UnbanCommand.js
@@ -1,11 +1,34 @@
 const BaseCommand = require('../../utils/structures/BaseCommand');
+const Discord = require('discord.js');
 
 module.exports = class UnbanCommand extends BaseCommand {
   constructor() {
     super('unban', 'moderation', []);
   }
 
-  run(client, message, args) {
-    message.channel.send('unban command works');
+  async run(client, message, args) {
+    //Permission Checks
+    if (!message.member.hasPermission("BAN_MEMBERS")) return message.channel.send("You do not have the required permissions to use this command.").then(msg => msg.delete({timeout: 7000})).then(message.delete({timeout: 7000}));
+    if (!message.guild.me.hasPermission("BAN_MEMBERS")) return message.channel.send("Bot does not have the requires permissions to use this command.").then(msg => msg.delete({timeout: 7000})).then(message.delete({timeout: 7000}));
+    //Variables
+    let reason = args.slice(1).join(" ");
+    let userID = args[0];
+    //Input Checks
+    if (!reason) reason = "No reason was provided.";
+    if (!args[0]) return message.channel.send("You must mention a user to unban. \`.unban ID reason\`").then(msg => msg.delete({timeout: 7000})).then(message.delete({timeout: 7000}));
+    if (!isNaN(args[0])) return message.channel.send("Please enter a valid user ID. \`.unban ID reason\`").then(msg => msg.delete({timeout: 7000})).then(message.delete({timeout: 7000}));
+    //Executing
+   message.guild.fetchBans().then(async bans => {
+     if (bans.size == 0) return message.channel.send("There are currently no bans in this sever.").then(msg => msg.delete({timeout: 7000})).then(message.delete({timeout: 7000}));
+     let bannedUser = bans.find(b => b.user.id == userID);
+     if (!bannedUser) return message.channel.send("That user ID is not banned from this server").then(msg => msg.delete({timeout: 7000})).then(message.delete({timeout: 7000}));
+     await message.guild.members.unban(bannedUser.user, reason).catch(err => {
+       console.log(err);
+       return message.channel.send("ERROR 404: something went wrong with unbanning that user ID.").then(msg => msg.delete({timeout: 7000})).then(message.delete({timeout: 7000}));
+     }).then(() => {
+       message.channe.send(`Unbanned ${arg[0]}`);
+     });
+   });
+
   }
 }
