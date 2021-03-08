bf40ac3c04b9696b04816180b89e7b9543cc10a7
bopus <65352524+bopus@users.noreply.github.com>
2021-03-08 12:43:12 -0500

    added lock and unlock commands

---
+++ src/commands/moderation/UnlockCommand.js
@@ -0,0 +1,22 @@
+const BaseCommand = require('../../utils/structures/BaseCommand');
+const Discord = require('discord.js');
+
+module.exports = class UnlockCommand extends BaseCommand {
+  constructor() {
+    super('unlock', 'moderation', []);
+  }
+
+  async run(client, message, args) {
+    if (!message.member.hasPermission("MANAGE_CHANNELS")) return message.channel.send('You do not have the required permissions to use this command.');
+    if (!message.guild.me.hasPermission("MANAGE_CHANNELS")) return message.channel.send('Zodex Bot requires \`MANAGE_CHANNELS\` permissions.');
+
+    const role = message.guild.roles.cache.get('817875564060475404');
+    let lockChannel = message.mentions.channels.first() || message.guild.channels.cache.get(args[0]);
+    if (!lockChannel) lockChannel = message.channel;
+
+    await lockChannel.updateOverwrite(role, {
+      SEND_MESSAGES: true
+    }).catch(err => console.log(err));
+    message.channel.send('Channel has been unlocked :unlock: ');
+  }
+}
