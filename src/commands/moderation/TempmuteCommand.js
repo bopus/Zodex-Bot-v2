4e78a3ba02ce4841e5acac765cb7fd9148f8ca5b
bopus <65352524+bopus@users.noreply.github.com>
2021-03-08 09:34:47 -0500

    added unmute but there is a big where bot responds and takes in commands multople

---
+++ src/commands/moderation/TempmuteCommand.js
@@ -0,0 +1,48 @@
+const BaseCommand = require('../../utils/structures/BaseCommand');
+const Discord = require('discord.js');
+const ms =require('ms');
+
+module.exports = class TempmuteCommand extends BaseCommand {
+  constructor() {
+    super('tempmute', 'moderation', []);
+  }
+
+  async run(client, message, args) {
+    if (!message.member.hasPermission("MUTE_MEMBERS")) return message.channel.send('You do not have the required permissions.');
+    if (!message.guild.me.hasPermission("MANAGE_ROLES")) return message.channel.send('Zodex Bot requires \`MANAGE_ROLES\` permissons.');
+
+    const muteRole = message.guild.roles.cache.get('816513027607756801');
+    const memberRole = message.guild.roles.cache.get('817875564060475404');
+    const mentionedMember = message.mentions.members.first() || message.guild.members.cache.get(args[0]);
+    const taggedUser = message.mentions.users.first();
+    let time = args[1];
+    let reason = args.slice(2).join(" ");
+    const tempmuteEmbed = new Discord.MessageEmbed()
+      .setTitle(`You were muted in ${message.guild.name}.`)
+      .addField(`Duration: ${time}`, `Reason: ${reason}`)
+      .setColor("#c377e0")
+      .setTimestamp();
+    const publicTempmuteEmbed = new Discord.MessageEmbed()
+      .setTitle(`${taggedUser} has been temp muted.`)
+      .addField(`Duration: ${time}`, `Reason: ${reason}`)
+      .setColor("#c377e0")
+      .setTimestamp();
+    
+    if (!args[0]) return message.channel.send('You need to declare a user, time and reason. \`.tempmute @user time reason\`');
+    if (!mentionedMember) return message.channel.send('That user is not in this server.');
+    if (!mentionedMember.roles.highest.position >= message.member.roles.highest.position) return message.channel.send('That user cannot be muted');
+    if (!time) return message.channel.send('You need to declare a duration of time. \`.tempmute @user time reason\`');
+    if (!reason) reason = 'No reason was provided';
+
+    await mentionedMember.roles.add(muteRole.id).catch(err => console.log(err));
+    await mentionedMember.roles.remove(memberRole.id).catch(err => console.log(err));
+    await mentionedMember.send(tempmuteEmbed).catch(err => console.log(err));
+    await message.channel.send(publicTempmuteEmbed).catch(err => console.log(err));
+
+    setTimeout(async function () {
+      await mentionedMember.roles.add(muteRole.id).catch(err => console.log(err));
+      await mentionedMember.roles.remove(memberRole.id).catch(err => console.log(err));
+      await mentionedMember.send(`You have been unmuted in ${message.guild.name}`).catch(err => console.log(err));
+    }, ms(time));
+  }
+}
